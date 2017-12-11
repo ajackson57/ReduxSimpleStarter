68ed185774cc2065724cf1877c4441c287416038
ajackson57 <ajackson5759@gmail.com>
2017-12-11 11:45:35 -0500

    Setup basic building blocks
    Setup src folder with index.js file, also create skeleton files for
    components.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyB8AK83UPAFaGyDFJv8PZmGnNWr6JkW-Mc';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// create a new component to produce html
+const App = function() {
+  return <div>Hi!</div>;
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take components generated html and put it in the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
